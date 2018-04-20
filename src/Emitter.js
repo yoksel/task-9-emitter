899ea251a200adad106f2a557924a6d88ba933cc
yokselzok <yokselzok@gmail.com>
2018-04-20 23:50:37 +0300

    Change events list to Map

--- src/Emitter.js
+++ src/Emitter.js
@@ -6,17 +6,17 @@ class Emitter {
 
     on(eventName, handler) {
         if (!this.events[eventName]) {
-            this.events[eventName] = [];
+            this.events[eventName] = new Map();
         }
 
-        this.events[eventName].push(handler);
+        this.events[eventName].set(handler, handler);
     }
 
     off(eventName, handler) {
         let events = this.events[eventName];
 
         if (events) {
-            this.events[eventName] = events.filter(func => func !== handler);
+            this.events[eventName].delete(handler)
         }
     }
 
